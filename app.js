0447cb61d8faca34e14cc6dae8b8c05356366669
Тимчасовий обліковий запис Student <student@kpnu.km.ua>
2018-03-03 18:36:12 +0200

    Flags of contries

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var browserify = require('browserify-middleware');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -21,6 +22,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+//app.get('/javascripts/bundle.js', browserify('./client/script.js'));
+
+if (app.get('env') == 'development') {
+  var browserSync = require('browser-sync');
+  var config = {
+    files: ["public/**/*.{js,css}", "client/*.js", "sass/**/*.scss", "views/**/*.hbs"],
+    logLevel: 'debug',
+    logSnippet: false,
+    reloadDelay: 3000,
+    reloadOnRestart: true
+  };
+  var bs = browserSync(config);
+  app.use(require('connect-browser-sync')(bs));
+}
 
 app.use('/', index);
 app.use('/users', users);
